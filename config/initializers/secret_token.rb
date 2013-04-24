d01e816a54e2a40cdcea121f47a5b488bd7c8f7c
Franco Brusatti <franco@mojotech.com>
2013-04-24 12:23:17 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fulbo::Application.config.secret_token = '1682662f95e1064e14a519302de3c0820bcbff371863312519b5df2be3a72404a6b09a650f2314efcc0b5ccbe681024deeb630a080f83f4bffa68d4098ee622b'
