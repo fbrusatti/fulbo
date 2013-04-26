602ae551140dd0390062297313a576b877bf00bd
edu <edu.depetris.00@gmail.com>
2013-04-26 07:45:57 -0300

    add ActiveAdmin

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ end
 
 module Fulbo
   class Application < Rails::Application
+
+    # ActiveAdmin
+    # If you are deploying Rails 3.1+ on Heroku, you may want to set:
+    config.assets.initialize_on_precompile = false
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Fulbo::Application.configure do
+
+  # ActiveAdmin
+  # In production, :host should be set to the actual host of your application.
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
