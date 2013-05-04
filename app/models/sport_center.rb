c3dedaae49f7c56b969299499c3d5ca82d9f44f8
Agustin <agvarea@gmail.com>
2013-05-04 09:11:49 -0300

    Merged validates on sport_center, remove downcase of attr and remove on_create of validate format of email

--- app/models/sport_center.rb
+++ app/models/sport_center.rb
@@ -1,16 +1,9 @@
 class SportCenter < ActiveRecord::Base
   attr_accessible :address, :cuit, :description, :email, :name, :phone
-  before_save { |sport_center| sport_center.email = email.downcase }
-  before_save { |sport_center| sport_center.description = description.downcase }
-  before_save { |sport_center| sport_center.address = address.downcase }
-  before_save { |sport_center| sport_center.name = name.downcase }
 
-  validates( :name, presence: true, length: {maximum: 40} )
-  validates( :email, presence: true )
-  validates( :address, presence: true )
-  validates( :description, presence: true, length: {maximum: 100} )
-  validates( :phone, presence: true, length: {maximum: 20} )
-  validates( :cuit, presence: true )
-
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => :create
+  validates_presence_of :address, :cuit, :description, :email, :name, :phone
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/
+  validates :name, length: {maximum: 40}
+  validates :description, length: {maximum: 100}
+  validates :phone, length: {maximum: 20}
 end
