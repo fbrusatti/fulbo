68777a9a96ed01d1237897b72ae67475c47a5df4
edu <edu.depetris.00@gmail.com>
2013-05-09 17:47:50 -0300

    add user_profile test

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -11,5 +11,27 @@
 require 'spec_helper'
 
 describe UserProfile do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it { should belong_to(:user) }
+  it { should have_db_column(:user_id).of_type(:integer)}
+
+  it { should allow_mass_assignment_of(:name) }
+  it { should allow_mass_assignment_of(:surname) }
+  it { should allow_mass_assignment_of(:nickname) }
+  it { should allow_mass_assignment_of(:playing_position) }
+  it { should allow_mass_assignment_of(:born) }
+  it { should allow_mass_assignment_of(:locality) }
+  it { should allow_mass_assignment_of(:foot) }
+  it { should allow_mass_assignment_of(:avatar) }
+  it { should allow_mass_assignment_of(:features) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:surname) }
+  it { should validate_presence_of(:playing_position) }
+
+  values = %w(Goalkeeper Defender Midfielder Forward Coach Coaching staff Referee Assistant referee)
+  it { should allow_value(values).for(:playing_position) }
+  it { should ensure_length_of(:features).is_at_most(200) }  
+
+
 end
