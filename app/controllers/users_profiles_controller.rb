bda4ddabc1a0a1fb6744863265b520d3ec161ba8
edu <edu.depetris.00@gmail.com>
2013-05-12 12:07:43 -0300

    modify show and edit views

--- app/controllers/users_profiles_controller.rb
+++ app/controllers/users_profiles_controller.rb
@@ -1,22 +1,34 @@
 class UsersProfilesController < ApplicationController
+  # for respond_with 
+  # respond_to :html
 
   # the profile is available only if user is authenticates 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!#, :only[:new, :show, :edit]
 
-  # POST /users_profiles
-  def create
+ 
+   # GET /users_profiles/:id
+  def show
+    @profile = UserProfile.find(params[:id])
   end
 
   # GET /users_profiles/new
   def new
+    @profile = UserProfile.new
   end
   
+
+  # POST /users_profiles
+  def create
+    @profile = current_user.build_user_profile(params[:user_profile])
+    if @profile.save
+      redirect_to users_profile_path(@profile), success: "Successfully created profile."
+    else
+      render "new"
+    end
+  end
+
   # GET /users_profiles/:id/edit
   def edit
-  end
-  
-  # GET /users_profiles/:id
-  def show
   end
   
   # PUT /users_profiles/:id
