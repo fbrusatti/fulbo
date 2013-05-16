7330da5f1f1f1939b4cde1c1e4381b555b8f56d4
edu <edu.depetris.00@gmail.com>
2013-05-16 12:04:36 -0300

    adapted the user profile to public and modification the users_profiles_controller code. I added  migration to use friendly

--- app/controllers/users_profiles_controller.rb
+++ app/controllers/users_profiles_controller.rb
@@ -3,40 +3,46 @@ class UsersProfilesController < ApplicationController
   respond_to :html
 
   # the profile is available only if user is authenticates 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:edit, :update, :create, :new] 
   before_filter :verify_profile, only: [:new]
  
    # GET /users/:user_id/profile
   def show
-    @profile = current_user.user_profile
+    @user = User.find(params[:user_id])
+    @profile = @user.user_profile
+    # if request.path != user_profile_path(@user)
+    #   redirect_to @profile, status: :moved_permanently
+    # end
   end 
 
   # GET /users/:user_id/profile/new
   def new
     @profile = UserProfile.new
   end
-  
+
+  # respond_with user_profile_path(@user)  
   # POST /users/:user_id/profile
   def create
-    @profile = current_user.build_user_profile(params[:user_profile])
+    @user = User.find(params[:user_id])
+    @profile = @user.build_user_profile(params[:user_profile])
     if @profile.save
       flash[:success] = "Successfully created profile."
     end
-    respond_with @profile
+    respond_with @profile, :location => user_profile_path
   end
 
   # GET /users_profiles/:id/edit
   def edit
-    @profile = current_user.user_profile
+    @profile = UserProfile.find_by_user_id(params[:user_id])
   end
   
   # PUT /users/:user_id/profile
   def update
-    @profile = current_user.user_profile
+    @profile = UserProfile.find_by_user_id(params[:user_id])
     if @profile.update_attributes(params[:user_profile])
       flash[:notice] = "Successfully updated profile."
     end
-    respond_with @profile 
+    respond_with @profile, :location => user_profile_path
   end
 
   private
