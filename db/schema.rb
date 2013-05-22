525efbef84c99d1b59945a4511fba90bb7ae756c
Agustin <agvarea@gmail.com>
2013-05-22 11:37:34 -0300

    Login with facebook

--- db/schema.rb
+++ db/schema.rb
@@ -93,10 +93,10 @@ ActiveRecord::Schema.define(:version => 20130522123352) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "slug"
-    t.string   "name"
     t.string   "provider"
     t.string   "uid"
+    t.string   "slug"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
