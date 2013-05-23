1bd19566145c0339ab593d39862ed8c7d970ea1c
edu <edu.depetris.00@gmail.com>
2013-05-23 18:29:33 -0300

    add teams controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Fulbo::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  resources :teams 
+
   devise_for :users,  :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users, :path => "" do
     resource :profile, controller: "users_profiles"
