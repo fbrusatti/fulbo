1e478c34e30f88ad992d6f42316b6784eddae367
Agustin <agvarea@gmail.com>
2013-05-23 22:09:17 -0300

    Add data of user on profile after signed in with facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -29,7 +29,11 @@ private
                     :token => access_token['credentials']['token'],
                     :secret => nil,
                     :name => access_token['extra']['raw_info']['name'],
-                    :link => access_token['extra']['raw_info']['link'] }
+                    :link => access_token['extra']['raw_info']['link'],
+                    :nickname => access_token['info']['nickname'],
+                    :location => access_token['info']['location'],
+                    :dob =>  Date.strptime(access_token['extra']['raw_info']['birthday'],'%m/%d/%Y'),
+                    :image => access_token['info']['image'] }
     when "Twitter"
       uid = access_token['extra']['user_hash']['id']
       name = access_token['user_info']['name']
@@ -38,16 +42,12 @@ private
                     :secret => access_token['credentials']['secret'], 
                     :name => name, 
                     :link => "http://twitter.com/#{name}" }
-    when 'LinkedIn'
-      uid = access_token['uid']
-      name = access_token['user_info']['name']
-      auth_attr = { :uid => uid, :token => access_token['credentials']['token'], :secret => access_token['credentials']['secret'], :name => name, :link => access_token['user_info']['public_profile_url'] }
     else
       raise 'Provider #{provider} not handled'
     end
     if resource.nil?
       if email
-        user = find_for_oauth_by_email(email, resource)
+        user = find_for_oauth_by_email(email, resource, auth_attr)
       elsif uid && name
         user = find_for_oauth_by_uid(uid, resource)
         if user.nil?
@@ -64,7 +64,6 @@ private
       user.authorizations << auth
     end
     # auth.update_attributes auth_attr
-
     return user
   end
 
@@ -76,24 +75,40 @@ private
     return user
   end
 
-  def find_for_oauth_by_email(email, resource=nil)
+  def find_for_oauth_by_email(email, resource=nil, auth_attr)
     if user = User.find_by_email(email)
       user
     else
-      user = User.new(:email => email, :password => Devise.friendly_token[0,20])
+      user = User.new(:email => email, 
+                      :password => Devise.friendly_token[0,20])
       user.save
+      profile_attr = auth_attr[:name].split(' ')
+      user.profile.update_attributes( :name => profile_attr[0], 
+                                      :surname => profile_attr[1], 
+                                      :nickname => auth_attr[:nickname],
+                                      :locality => auth_attr[:location],
+                                      :dob => auth_attr[:dob],
+                                      :avatar => auth_attr[:image] )
     end
     return user
   end
 
-  def find_for_oauth_by_name(name, resource=nil)
+  def find_for_oauth_by_name(name, resource=nil, auth_attr)
     if user = User.find_by_name(name)
       user
     else
-      user = User.new(:name => name, :password => Devise.friendly_token[0,20], :email => "#{UUIDTools::UUID.random_create}@host")
+      user = User.new(:name => name, 
+                      :password => Devise.friendly_token[0,20], 
+                      :email => "#{UUIDTools::UUID.random_create}@host")
       user.save false
+      profile_attr = auth_attr[:name].split(' ')
+      user.profile.update_attributes( :name => profile_attr[0], 
+                                      :surname => profile_attr[1], 
+                                      :nickname => auth_attr[:nickname],
+                                      :locality => auth_attr[:location],
+                                      :dob => auth_attr[:dob],
+                                      :avatar => auth_attr[:image] )
     end
     return user
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -104,9 +104,9 @@ ActiveRecord::Schema.define(:version => 20130522123352) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "slug"
     t.string   "provider"
     t.string   "uid"
+    t.string   "slug"
     t.string   "name"
   end
 
