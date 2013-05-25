0a3cb82237ebabc955f9e8df8878d5a7a89ee78f
edu <edu.depetris.00@gmail.com>
2013-05-25 20:30:36 -0300

    add the team controller and I added its views. I put the  holder.js file  to include images along with Bootstrap and  I include to this  in assets/javascripts and assets/stylesheets also I changed in the helper the method that shows errors

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,39 @@
 class TeamsController < ApplicationController
+  respond_to :html
+
+  before_filter :verfify_team, only: [:new]
+
+  def index
+    @teams = Team.all
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = current_user.create_team(params[:team])
+    if @team.save
+      flash[:success] = "Successfully created team."
+    end
+    respond_with @team
+  end
+
+  def show
+    @team = Team.find(params[:id])
+  end
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    flash[:notice] = "Successfully destroyed team."
+    respond_with(@team)
+  end
+
+  private
+
+    def verfify_team
+      redirect_to teams_path, notice: "you has a team" unless current_user.team.blank?
+    end
+
 end
