32c74fcd5926a99ab5ac72dd852f9d148aad88c4
edu <edu.depetris.00@gmail.com>
2013-05-27 15:45:24 -0300

    add friendlyId to team

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130523124717) do
+ActiveRecord::Schema.define(:version => 20130527114915) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -94,9 +94,11 @@ ActiveRecord::Schema.define(:version => 20130523124717) do
     t.integer  "owner_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "slug"
   end
 
   add_index "teams", ["owner_id"], :name => "index_teams_on_owner_id"
+  add_index "teams", ["slug"], :name => "index_teams_on_slug"
 
   create_table "user_profiles", :force => true do |t|
     t.integer  "user_id"
@@ -125,9 +127,9 @@ ActiveRecord::Schema.define(:version => 20130523124717) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "slug"
     t.string   "provider"
     t.string   "uid"
-    t.string   "slug"
     t.string   "name"
   end
 
