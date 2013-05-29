57a249fe8e7e4d7cc908dc83b6fed7d0fe381dc6
edu <edu.depetris.00@gmail.com>
2013-05-29 09:11:47 -0300

    Create team_profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130527114915) do
+ActiveRecord::Schema.define(:version => 20130528125635) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -75,6 +75,17 @@ ActiveRecord::Schema.define(:version => 20130527114915) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "owner_id"
+  end
+
+  create_table "team_profiles", :force => true do |t|
+    t.integer  "team_id"
+    t.string   "category"
+    t.string   "surface"
+    t.string   "geo_availability"
+    t.string   "badge"
+    t.string   "description"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
   create_table "team_users", :force => true do |t|
