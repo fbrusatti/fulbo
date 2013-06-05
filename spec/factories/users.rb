f0631470740b1a3c81286879884c4f3de0211284
Franco Brusatti <franco@mojotech.com>
2013-06-05 10:11:55 -0300

    Improve factory to users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
-  factory :user do
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
 
+  factory :user do
+    sequence(:name)  { |n| "User N#{n}" }
+    email            { FactoryGirl.generate :email }
+    password         'password'
   end
 end
