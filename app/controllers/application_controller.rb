f13e611d8b2f4af8b1b92ec93cd43bf4e081d425
Franco Brusatti <franco@mojotech.com>
2013-06-09 23:37:59 -0300

    Prepare AppController to get locale from request.header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,17 @@ class ApplicationController < ActionController::Base
       root_path
     end
   end
+
+  private
+  def set_locale
+    if language_request = request.env["HTTP_ACCEPT_LANGUAGE"]
+      language   = extract_locale_from_accept_language_header(language_request)
+      @locale     = language.blank? ? params[:lang] : language
+      I18n.locale = @locale
+    end
+  end
+
+  def extract_locale_from_accept_language_header(http_var)
+    http_var.scan(/^[a-z]{2}/).first
+  end
 end
