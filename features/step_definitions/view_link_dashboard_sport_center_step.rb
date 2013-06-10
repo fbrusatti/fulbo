a1395367c5d8de744852688127a1df551fb5cdc5
Rodrigo Arino <rodrigoarino@gmail.com>
2013-06-10 14:22:35 -0300

    factored code

--- features/step_definitions/view_link_dashboard_sport_center_step.rb
+++ features/step_definitions/view_link_dashboard_sport_center_step.rb
@@ -4,15 +4,16 @@ Given(/^I am logged in$/) do
 	visit new_user_session_path
 	@user = FactoryGirl.create(:user)
 	fill_in 'user_email', with: @user.email
-  fill_in 'user_password', with: "password"
+	fill_in 'user_password', with: "password"
 	click_button 'Sign in'
 end
 
-When(/^I am owner to sport center$/) do
-  @user.sport_center = FactoryGirl.create(:sport_center)
+When(/^I am( not)? owner to sport center$/) do |negation|
+  @user.sport_center = negation.present? ? nil : FactoryGirl.create(:sport_center)
 end
 
-Then(/^I should view link dashboard sport center$/) do
+Then(/^I should( not)? view link dashboard sport center$/) do |negation|
 	visit root_path
-	page.should have_content "Complejo"
+	expectation = negation.present? ? :should_not : :should
+	page.send(expectation, have_content("Complejo"))
 end
