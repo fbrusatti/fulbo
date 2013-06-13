be4cf49eec6903db129e226dbba7b4be78369be4
edu <edu.depetris.00@gmail.com>
2013-06-13 14:09:16 -0300

    Add I18n to errors_form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,13 +6,13 @@ module ApplicationHelper
       html << "<div class='formErrors #{object.class.name.humanize.downcase}Errors'>\n"
       if message.blank?
         if object.new_record?
-          html << "\t\t<h5>There was a problem creating the #{object.class.name.humanize.downcase}</h5>\n"
+          html << "\t\t<h5> #{t("flash.error_create_form")} </h5>\n"
         else
-          html << "\t\t<h5>There was a problem updating the #{object.class.name.humanize.downcase}</h5>\n"
-        end    
+          html << "\t\t<h5> #{t("flash.error_update_form")} </h5>\n"
+        end
       else
         html << "<h5>#{message}</h5>"
-      end  
+      end
       html << "\t\t<ul>\n"
       object.errors.full_messages.each do |error|
         html << "\t\t\t<li>#{error}</li>\n"
