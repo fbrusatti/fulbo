f915bbe16ba0441b41c9643fa26108181eb26a2e
saul coria <coriasaul@gmail.com>
2013-06-13 16:08:20 -0300

    Add migrations to create a location

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612172502) do
+ActiveRecord::Schema.define(:version => 20130613163230) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -92,6 +92,17 @@ ActiveRecord::Schema.define(:version => 20130612172502) do
   end
 
   add_index "leagues", ["organizer_id"], :name => "index_leagues_on_organizer_id"
+
+  create_table "locations", :force => true do |t|
+    t.integer  "sport_center_id"
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "locality"
+    t.string   "description"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
   create_table "photos", :force => true do |t|
     t.string   "name"
@@ -183,6 +194,8 @@ ActiveRecord::Schema.define(:version => 20130612172502) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "provider"
+    t.string   "uid"
     t.string   "slug"
     t.string   "name"
   end
