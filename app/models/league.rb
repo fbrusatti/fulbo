74d9db504d5abba3e7a5088e6728c76260b90bfe
edu <edu.depetris.00@gmail.com>
2013-06-19 10:46:05 -0300

    small adjustments in models League, Team and PointSystem

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,15 +1,16 @@
 class League < ActiveRecord::Base
 
   # == Accessors
-  attr_accessible :name, :category, :number_matches, :field_price, :description,
-                  :registration_price, :requirements, :number_teams, :start_date
+  attr_accessible :name, :category, :number_matches, :field_price, :registration_price,
+                  :requirements, :number_teams, :start_date, :point_system_attributes,
+                  :affiliations_attributes, :teams_attributes
 
   # == Validations
   validates_presence_of :name, :category, :start_date
 
   # == Associations
   belongs_to :organizer, class_name: 'SportCenter'
-  has_one :point_system, dependent: :destroy
+  has_one :point_system, dependent: :destroy, inverse_of: :league
   has_many :affiliations, dependent: :destroy
   has_many :teams, through: :affiliations
 
@@ -17,7 +18,12 @@ class League < ActiveRecord::Base
   before_create :init_point_system
 
   # == Nested Attributes
-  accepts_nested_attributes_for :point_system
+  accepts_nested_attributes_for :point_system, :affiliations
+  accepts_nested_attributes_for :teams, allow_destroy: true
+
+  # == Serializes
+  serialize :category, Array
+
 
   private
     def init_point_system
