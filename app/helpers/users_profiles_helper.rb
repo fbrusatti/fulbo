012f6614cf02e1f1bca19ccf2c84de4a603e5cc1
Franco Brusatti <fbrusatti@gmail.com>
2013-07-02 01:15:45 -0300

    Use abstraction on views to clean user profile partials

--- app/helpers/users_profiles_helper.rb
+++ app/helpers/users_profiles_helper.rb
@@ -1,6 +1,25 @@
 module UsersProfilesHelper
 
   POSITIONS = %w{Goalkeeper Defender Midfielder Forward Coach Coaching Staff Referee Assistant }
-  FOOTS = %w{right left ambidextro  }
+  FOOTS = %w{right left ambidextro}.map{ |key| I18n.t("user_profile.#{key}")}
+
+  def user_profile_positions(form)
+    label = content_tag :label,
+                        I18n.t('user_profile.field_position')
+
+    options = ""
+    UsersProfilesHelper::POSITIONS.each do |position|
+      check_box = form.check_box :playing_position,
+                                 {:multiple => true},
+                                 position,
+                                 nil
+      position_label = form.label position
+
+      options << check_box + position_label
+    end
+
+    options_div = content_tag :div, options.html_safe, class: 'user-profile-positions'
+    label + options_div
+  end
 end
 
