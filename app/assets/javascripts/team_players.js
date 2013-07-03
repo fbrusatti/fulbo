8b161a8523a561ab9ff113d7d547982aa4dc1f70
Franco Brusatti <fbrusatti@gmail.com>
2013-07-03 12:55:25 -0300

    Get working buttons in show teams template

--- app/assets/javascripts/team_players.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $('#team_player_tokens').tokenInput('/users.json', { 
-    crossDomain: false,
-    prePopulate: $('#team_player_tokens').data('pre'),
-    theme: "facebook",
-    preventDuplicates: true
-  });
-});
