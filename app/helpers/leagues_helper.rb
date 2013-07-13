3d57e10e2851d6dfc726de725e7ad8e49f58e26f
edu <edu.depetris.00@gmail.com>
2013-07-13 15:30:42 -0300

    Add semantics to Show of League

--- app/helpers/leagues_helper.rb
+++ app/helpers/leagues_helper.rb
@@ -20,12 +20,12 @@ module LeaguesHelper
       btn_edit = content_tag(:div, class: "btn-group" ) do
         button_to t(".btn_edit"), edit_league_path(@league),
                                               method: :get,
-                                              class: "btn"
+                                              class: "edit-button"
       end
       btn_delete = content_tag(:div, class: "btn-group" ) do
         button_to t(".btn_delete"), league_path(@league),
                                                 method: :delete,
-                                                class: "btn btn-danger",
+                                                class: "delete-button",
                                                 confirm: t(".confirm_delete_msg")
       end
       btn_edit + btn_delete
@@ -34,7 +34,7 @@ module LeaguesHelper
 
   def buttons_owner_team(league)
     if is_owner_of_team? && has_sent_request_to_league?(@league)
-      btn = content_tag(:div, class: "btn btn-primary disabled") do
+      btn = content_tag(:div, class: "affiliation-sent-button") do
         t(".has_been_sent_affiliation")
       end
       btn and return
@@ -42,7 +42,7 @@ module LeaguesHelper
     if is_owner_of_team? && !playing_league?(league)
       field_requested = hidden_field_tag :requested_id, league.id
       field_requested_type = hidden_field_tag :requested_type, league.class.name
-      button = submit_tag t(".btn_send_affiliation"), class: "btn btn-primary"
+      button = submit_tag t(".btn_send_affiliation"), class: "send-affiliation-button"
       form = form_for(@request_inscription,
                      url: team_request_inscriptions_path(current_user.team, league),
                      remote: true) do |f|
